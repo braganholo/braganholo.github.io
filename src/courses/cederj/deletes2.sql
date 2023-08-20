fe630be4fee8b0be7bd12e1d810a8d1e8b18857c
Vanessa Braganholo Murta <vanessa@chinchila.local>
2023-08-19 23:02:36 -0300

    Migrates the website

---
+++ src/courses/cederj/deletes2.sql
@@ -0,0 +1,39 @@
+﻿use emp;
+
+DROP TABLE EQUIPAMENTO;
+
+DELETE FROM DEPENDENTE;
+
+DELETE FROM TRABALHA_EM;
+
+DELETE FROM PROJETO;
+
+DELETE FROM DEPT_LOCALIZACOES;
+
+--
+-- EMPREGADO
+-- Primeiro setar todos os gerentes para NULL,
+-- para não dar problemas de integridade referencial
+-- ao excluir algum empregado que seja gerente de algum outro ainda não excluído
+--
+
+UPDATE EMPREGADO
+SET SUPERSSN=null;
+
+-- apaga tuplas de empregado
+
+DELETE FROM EMPREGADO;
+
+DELETE FROM DEPARTAMENTO;
+
+
+
+
+
+
+
+
+
+
+
+
